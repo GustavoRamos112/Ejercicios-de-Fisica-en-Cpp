aca36e7069d36dde75fee1542cbc0bf1da6aa7b6
GustavoRamos112 <gustavo.angel.fr.96@gmail.com>
2024-01-28 15:54:59 -0600

    Factorial de un numero natural (n!)

---
+++ Factorial.cpp
@@ -0,0 +1,36 @@
+//Itz El factorial de un numero entero positivo n se define como:
+//Itz n! = n*(n-1)*(n-2)*...*2*1.
+
+//* Indicaciones: Crear una funcion que al llamarla y darle como
+//* argumento un numero entero positivo, devuelva su factorial.
+#include <iostream>
+
+using namespace std;
+
+int factorial(int _numero);
+
+int main(void) {
+    int num = 5;
+    //? Llamamos a la funcion dandole como argumetno el numero que definimos
+    //? anteriormente, el 5 en este caso.
+    cout << num << "! = " << factorial(num);
+    return 0;
+}
+
+int factorial(int _numero) {
+    //? Si el numero no es positivo devuelve un error.
+    if(_numero < 0) {
+        cout << "Error, el numero tiene que ser un entero positivo\nCodigo de error: 1"; 
+        return 1;
+    }
+    //? Definimos el inicio de la multiplicacion.
+    int fac = 1;    
+    //? El ciclo lo empezamos en 2 pues no es necesario multiplicar por 1.
+    //? 1*2*3*...*(_numero-1)*_numero.
+    for(int i = 2; i <= _numero; i++) {
+        //? Se hace la multiplicacion
+        fac *= i;
+    }
+    //? Devolvemos el valor del factorial.
+    return fac;
+}
